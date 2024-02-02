12cfb46bebd43bf81fd29abd32b0cb3398856111
AhimsaGMS <ytahimsavidur@gmail.com>
2024-02-02 16:11:52 +0000

    Added Name Coord Fetch From Database

--- PHP API/api.php
+++ PHP API/api.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+require_once "dbconnect.php";
 
 function calculateX($fontSize, $fontPath, $text, $imageWidth, $offsetX = 0) {
     $textBoundingBox = imagettfbbox($fontSize, 0, $fontPath, $text);
@@ -24,9 +25,12 @@ $style = isset($_GET["style"]) ? $_GET["style"] : "Style 1";
 $company = isset($_GET["company"]) ? $_GET["company"] : "Default Company";
 $signedBy = isset($_GET["signedBy"]) ? $_GET["signedBy"] : "Default Signee";
 $dateOfIssue = isset($_GET["dateOfIssue"]) ? $_GET["dateOfIssue"] : date("F j, Y");
+$cert_name = isset($_GET["certname"]) ? $_GET["certname"] : "Default Certificate";
+
+
 
 // Path to the certificate template and font
-$imagePath = 'assets/certificate_of_appreciation/1.png'; // Make sure this path is correct
+$imagePath = 'assets/certificate_of_appreciation/appreciation_1.png'; // Make sure this path is correct
 $fontPath = 'fonts/ArianaVioleta.ttf'; // Make sure this path is correct and the file is readable
 $signedByFontPath = 'fonts/signedByFont.ttf'; // Path to the font for 'Signed By'
 
@@ -49,26 +53,33 @@ $imageHeight = imagesy($image);
 $fontSize = 100; // Adjust the font size as needed
 
 // Set the text color (black)
-$textColour = imagecolorallocate($image, 160,82,45);
+$query = "SELECT * FROM `certificate_db` WHERE certificate_name='$cert_name'";
+$query_run = mysqli_query($link, $query);
+while ($rows = mysqli_fetch_array($query_run)) 
+{
+    $textColour = imagecolorallocate($image, 160,82,45);
 
-// Name text
-$nameText = $name;
-// Set your desired offsets here
-$offsetX = 0; // Horizontal adjustment (left/right)
-$offsetY = 300; // Vertical adjustment (up/down)
-$x = calculateX($fontSize, $fontPath, $nameText, $imageWidth, $offsetX);
-$y = calculateY($fontSize, $fontPath, $nameText, $imageHeight, $offsetY);
+    // Gets the co-ordinates for the names from the DB
+    $name_cords = explode(",", trim($rows['name_loc'], "()")) ;
+    // Name text
+    $nameText = $name;
+    // Set your desired offsets here
+    $offsetX = $name_cords[0]; // Horizontal adjustment (left/right)
+    $offsetY = $name_cords[1]; // Vertical adjustment (up/down)
+    $x = calculateX($fontSize, $fontPath, $nameText, $imageWidth, $offsetX);
+    $y = calculateY($fontSize, $fontPath, $nameText, $imageHeight, $offsetY);
 
-imagettftext($image, $fontSize, 0, $x, $y, $textColour, $fontPath, $nameText);
+    imagettftext($image, $fontSize, 0, $x, $y, $textColour, $fontPath, $nameText);
 
-// // Signed by
-$signedByFontPath = 'fonts/ArianaVioleta.ttf'; // Path to the different font for 'Signed By'
-$signedByText = $signedBy;
-$offsetXSignedBy = 0; // Horizontal adjustment for Signed By
-$offsetYSignedBy = 1150; // Vertical adjustment for Signed By
-$xSignedBy = calculateX($fontSize, $signedByFontPath, $signedByText, $imageWidth, $offsetXSignedBy);
-$ySignedBy = calculateY($fontSize, $signedByFontPath, $signedByText, $imageHeight, $offsetYSignedBy);
-imagettftext($image, $fontSize, 0, $xSignedBy, $ySignedBy, $textColour, $signedByFontPath, $signedByText);
+    // // Signed by
+    $signedByFontPath = 'fonts/ArianaVioleta.ttf'; // Path to the different font for 'Signed By'
+    $signedByText = $signedBy;
+    $offsetXSignedBy = 0; // Horizontal adjustment for Signed By
+    $offsetYSignedBy = 1150; // Vertical adjustment for Signed By
+    $xSignedBy = calculateX($fontSize, $signedByFontPath, $signedByText, $imageWidth, $offsetXSignedBy);
+    $ySignedBy = calculateY($fontSize, $signedByFontPath, $signedByText, $imageHeight, $offsetYSignedBy);
+    imagettftext($image, $fontSize, 0, $xSignedBy, $ySignedBy, $textColour, $signedByFontPath, $signedByText);
+}
 
 // // Date of Issue
 // $dateOfIssueText = "Date of Issue: " . $dateOfIssue;

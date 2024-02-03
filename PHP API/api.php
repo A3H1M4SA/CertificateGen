0150e7d70bee43557e2aeefeb1cb7449e00e607c
AhimsaGMS <ytahimsavidur@gmail.com>
2024-02-03 16:03:07 +0000

    Fixed Best Employee and added Custom Get for Font Size and Color for every Field

--- PHP API/api.php
+++ PHP API/api.php
@@ -34,7 +34,7 @@ $query = "SELECT * FROM `certificate_db` WHERE certificate_name='$cert_name'";
 $query_run = mysqli_query($link, $query);
 while ($rows = mysqli_fetch_array($query_run)) 
 {
-    $imagePath = $rows['certificate_path'].'.png'; // Make sure this path is correct
+    $imagePath = "assets/certificate_of_appreciation/bestemployee.png"; // Make sure this path is correct
     $fontPath = $rows['primaryfont']; // Make sure this path is correct and the file is readable
     $signedByFontPath = $rows['primaryfont']; // Path to the font for 'Signed By
 }
@@ -62,43 +62,79 @@ $query_run = mysqli_query($link, $query);
 while ($rows = mysqli_fetch_array($query_run)) 
 {
 
-    // Gets the co-ordinates for the names from the DB
+    // Gets the co-ordinates for each from the DB
     $name_cords = explode(",", trim($rows['name_loc'], "()")) ;
-    $color_rgb = explode(",", trim($rows['text_color'], "()")) ;
     $signed_cords = explode(",", trim($rows['signedby_loc'], "()")) ;
     $company_cords = explode(",", trim($rows['company_loc'], "()")) ;
+    $date_cords = explode(",", trim($rows['date_loc'], "()")) ;
 
-    // Set text name and color
-    $nameText = $name;
-    $textColour = imagecolorallocate($image,  $color_rgb[0], $color_rgb[1], $color_rgb[2]);
+    // Gets the Color(RGB) Values for each from the DB
+    // $color_rgb = explode(",", trim($rows['text_color'], "()")) ;
+    // $othercolor_rgb = explode(",", trim($rows['othertext_color'], "()")) ;
+    $name_rgb = explode(",", trim($rows['name_textcolor'], "()")) ;
+    $signedby_rgb = explode(",", trim($rows['signedby_textcolor'], "()")) ;
+    $company_rgb = explode(",", trim($rows['company_textcolor'], "()")) ;
+    $date_rgb = explode(",", trim($rows['date_textcolor'], "()")) ;
 
-    // Define the font size
+    //Gets the Fontsize for each Values from the DB
     $fontSize_Name = $rows['name_fontsize'];
-    $fontSize_Other = $rows['other_fontsize'];
+    $fontSize_SignedBy = $rows['signedby_fontsize'];
+    $fontSize_Company = $rows['company_fontsize'];
+    $fontSize_Date = $rows['date_fontsize'];
 
-    // Set your desired offsets here
-    $offsetX = $name_cords[0]; // Horizontal adjustment (left/right)
-    $offsetY = $name_cords[1]; // Vertical adjustment (up/down)
-    $x = calculateX($fontSize_Name, $fontPath, $nameText, $imageWidth, $offsetX);
-    $y = calculateY($fontSize_Name, $fontPath, $nameText, $imageHeight, $offsetY);
 
-    imagettftext($image, $fontSize_Name, 0, $x, $y, $textColour, $fontPath, $nameText);
+    if($rows['name_loc'] != 'NIL')
+    {
+        // Set text name and color
+        $nameText = $name;
+        $textColour = imagecolorallocate($image,  $name_rgb[0], $name_rgb[1], $name_rgb[2]);
 
-    // Signed by
-    $signedByText = $signedBy;
-    $offsetXSignedBy = $signed_cords[0]; // Horizontal adjustment for Signed By
-    $offsetYSignedBy = $signed_cords[1]; // Vertical adjustment for Signed By
-    $xSignedBy = calculateX($fontSize_Other, $signedByFontPath, $signedByText, $imageWidth, $offsetXSignedBy);
-    $ySignedBy = calculateY($fontSize_Other, $signedByFontPath, $signedByText, $imageHeight, $offsetYSignedBy);
-    imagettftext($image, $fontSize_Other, 0, $xSignedBy, $ySignedBy, $textColour, $signedByFontPath, $signedByText);
+        // Set your desired offsets here
+        $offsetX = $name_cords[0]; // Horizontal adjustment (left/right)
+        $offsetY = $name_cords[1];; // Vertical adjustment (up/down)
+        $x = calculateX($fontSize_Name, $fontPath, $nameText, $imageWidth, $offsetX);
+        $y = calculateY($fontSize_Name, $fontPath, $nameText, $imageHeight, $offsetY);
 
-    // Company
-    $signedByText = $company;
-    $offsetXSignedBy = $company_cords[0]; // Horizontal adjustment for Signed By
-    $offsetYSignedBy = $company_cords[1]; // Vertical adjustment for Signed By
-    $xSignedBy = calculateX($fontSize_Other, $signedByFontPath, $signedByText, $imageWidth, $offsetXSignedBy);
-    $ySignedBy = calculateY($fontSize_Other, $signedByFontPath, $signedByText, $imageHeight, $offsetYSignedBy);
-    imagettftext($image, $fontSize_Other, 0, $xSignedBy, $ySignedBy, $textColour, $signedByFontPath, $signedByText);
+        imagettftext($image, $fontSize_Name, 0, $x, $y, $textColour, $fontPath, $nameText);
+    }
+
+    if($rows['signedby_loc'] != 'NIL')
+    {
+        // Signed by
+        $textColour_SignedBy = imagecolorallocate($image,  $signedby_rgb[0], $signedby_rgb[1], $signedby_rgb[2]);
+        $signedByText = $signedBy;
+        $offsetXSignedBy = $signed_cords[0]; // Horizontal adjustment for Signed By
+        $offsetYSignedBy = $signed_cords[1]; // Vertical adjustment for Signed By
+        $xSignedBy = calculateX($fontSize_SignedBy, $signedByFontPath, $signedByText, $imageWidth, $offsetXSignedBy);
+        $ySignedBy = calculateY($fontSize_SignedBy, $signedByFontPath, $signedByText, $imageHeight, $offsetYSignedBy);
+        imagettftext($image, $fontSize_SignedBy, 0, $xSignedBy, $ySignedBy, $textColour_SignedBy, $signedByFontPath, $signedByText);
+    }
+
+    if($rows['company_loc'] != 'NIL')
+    {
+
+        // Company
+        $textColour_Company = imagecolorallocate($image,  $company_rgb[0], $company_rgb[1], $company_rgb[2]);
+        $companyByText = $company;
+        $offsetXSignedBy = $company_cords[0]; // Horizontal adjustment
+        $offsetYSignedBy = $company_cords[1]; // Vertical adjustment
+        $xSignedBy = calculateX($fontSize_Company, $signedByFontPath, $companyByText, $imageWidth, $offsetXSignedBy);
+        $ySignedBy = calculateY($fontSize_Company, $signedByFontPath, $companyByText, $imageHeight, $offsetYSignedBy);
+        imagettftext($image, $fontSize_Company, 0, $xSignedBy, $ySignedBy, $textColour_Company, $signedByFontPath, $companyByText);
+    }
+
+    if($rows['date_loc'] != 'NIL')
+    {
+        // Date
+        $textColour_Date = imagecolorallocate($image,  $date_rgb[0], $date_rgb[1], $date_rgb[2]);
+        $datetext = $company;
+        $offsetXSignedBy = $date_cords[0]; // Horizontal adjustment
+        $offsetYSignedBy = $date_cords[1]; // Vertical adjustment
+        $xSignedBy = calculateX($fontSize_Date, $signedByFontPath, $datetext, $imageWidth, $offsetXSignedBy);
+        $ySignedBy = calculateY($fontSize_Date, $signedByFontPath, $datetext, $imageHeight, $offsetYSignedBy);
+        imagettftext($image, $fontSize_Date, 0, $xSignedBy, $ySignedBy, $textColour_Date, $signedByFontPath, $datetext);
+    }
+
 
     //Update API for Certificate
     $updatecertcount = "UPDATE certificate_db SET certificate_used=certificate_used+1 WHERE certificate_name='$cert_name'";

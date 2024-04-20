7ee611a5407405c78a0d25f5a877bd89cf10b54b
AhimsaGMS <ytahimsavidur@gmail.com>
2024-04-20 21:05:45 +0100

    Added Comments

--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -8,7 +8,7 @@ public class WelcomeScreen {
 
     public WelcomeScreen(ActionListener continueAction) {
         panel = new JPanel(new GridLayout(2, 1));
-        panel.setPreferredSize(new Dimension(600, 400)); // Suggest that the panel should be 800x600 pixels
+        panel.setPreferredSize(new Dimension(600, 400)); // Standard Size Panel should be 800x600 pixels
         JLabel welcomeLabel = new JLabel("Welcome To Certificate Generator", JLabel.CENTER);
         JButton continueButton = new JButton("Continue");
         continueButton.addActionListener(continueAction);

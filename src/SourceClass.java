abf2fb72aeba47418b114513958c71c36add7975
AhimsaGMS <ytahimsavidur@gmail.com>
2024-04-20 20:59:34 +0100

    Fixed The API and Preview API and added Comments

--- src/SourceClass.java
+++ src/SourceClass.java
@@ -1,9 +1,10 @@
 public class SourceClass {
     // API endpoints
-    public static final String API_BASE_URL = "http://localhost/CertificateGen/PHP%20API/";
+//    public static final String API_BASE_URL = "http://localhost/CertificateGen/PHP%20API/";
+    public static final String API_BASE_URL = "https://api.gmsahimsa.com/";
     public static final String PREVIEW_API_URL = API_BASE_URL + "preview.php";
     public static final String SAVE_API_URL = API_BASE_URL + "api.php";
-    public static final String THUMBNAIL_URL = "http://localhost/CertificateGen/src/Placeholder.jpg.";
+    public static final String THUMBNAIL_URL = "https://api.gmsahimsa.com/Placeholder.jpg";
 
     // Other configuration constants, if needed
 }
